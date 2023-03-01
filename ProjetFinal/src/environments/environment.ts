01fda70be13dae63d818caceab1e8f6a8c915353
MirkoNicola <103948551+MirkoNicola@users.noreply.github.com>
2023-03-01 22:13:11 +0300

    Angular/Firebase setuUp

---
+++ ProjetFinal/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment= {
+  firebase: {
+    projectId: 'projetmc-b0fa2',
+    appId: '1:1052240691743:web:fdb54001f291fe56180b2a',
+    storageBucket: 'projetmc-b0fa2.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBFxDv2S12AgefWHoyfO1OI0AHUOde5WzM',
+    authDomain: 'projetmc-b0fa2.firebaseapp.com',
+    messagingSenderId: '1052240691743',
+    measurementId: 'G-Y4B5K54QNB',
+  },
+
+}
